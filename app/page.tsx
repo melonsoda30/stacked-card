10b24b328ac47a0c616aa7e15f645af379d974ad
miftah <miftah.it2@gmail.com>
2025-06-23 21:33:35 +0700

    add prev, next, shuffle

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { Card } from "@/components/card";
+import { Shuffle, SkipForward } from "lucide-react";
 import { useState } from "react";
 
 export default function Home() {
-  const [reset, setReset] = useState(0);
-
-  function handleReset() {
-    setReset((prev) => prev + 1);
-  }
-  const colors = [
+  const [colors, setColors] = useState([
     "#0fa188",
     "#335981",
     "#b433d5",
@@ -19,7 +15,46 @@ export default function Home() {
     "#aa3184",
     "#757333",
     "#44863f",
-  ];
+  ]);
+  const [reset, setReset] = useState(0);
+  const [activeCard, setActiveCard] = useState(0);
+  const [direction, setDirection] = useState<
+    "prev" | "next" | "shuffle" | null
+  >(null);
+
+  const handleNext = () => {
+    setActiveCard((prev) => prev + 1);
+    setDirection("next");
+
+    if (activeCard === colors.length - 1) {
+      setTimeout(() => {
+        setDirection(null);
+        setActiveCard(0);
+        setReset((prev) => prev + 1);
+      }, 1000);
+    }
+  };
+
+  const handlePrev = () => {
+    setActiveCard((prev) => prev - 1);
+    setDirection("prev");
+  };
+
+  const handleShuffle = () => {
+    setDirection("shuffle");
+    const shuffledColors = [...colors].sort(() => Math.random() - 0.5);
+    setTimeout(() => {
+      setColors(shuffledColors);
+      setDirection(null);
+      setActiveCard(0);
+      setReset((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const handleReset = () => {
+    setReset((prev) => prev + 1);
+  };
+
   return (
     <div className="relative h-screen w-screen overflow-hidden bg-gray-600 flex flex-col items-center justify-center">
       {colors.map((bg, i) => (
@@ -30,8 +65,24 @@ export default function Home() {
           handleReset={handleReset}
           resetCount={reset}
           index={i}
+          activeCard={activeCard}
+          direction={direction}
+          setActiveCard={setActiveCard}
+          setDirection={setDirection}
         />
       ))}
+
+      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-row gap-10">
+        <button onClick={handlePrev} disabled={activeCard === 0}>
+          <SkipForward className="rotate-180" />
+        </button>
+        <button onClick={handleNext} disabled={activeCard === colors.length}>
+          <SkipForward />
+        </button>
+        <button onClick={handleShuffle} disabled={activeCard === colors.length}>
+          <Shuffle />
+        </button>
+      </div>
     </div>
   );
 }
--- components/card.tsx
+++ components/card.tsx
@@ -3,23 +3,33 @@ import { useMotionValue, animate, useTransform } from "motion/react";
 import { motion } from "framer-motion";
 import { useEffect, useMemo, useState } from "react";
 
+type Direction = "prev" | "next" | "shuffle" | null;
+
+interface CardProps {
+  bgColor: string;
+  isLast: boolean;
+  handleReset: () => void;
+  resetCount: number;
+  index: number;
+  activeCard: number;
+  direction: Direction;
+  setActiveCard: React.Dispatch<React.SetStateAction<number>>;
+  setDirection: React.Dispatch<React.SetStateAction<Direction>>;
+}
+
 export function Card({
   bgColor,
   isLast,
   handleReset,
   resetCount,
   index,
-}: {
-  bgColor: string;
-  isLast: boolean;
-  handleReset: () => void;
-  resetCount: number;
-  index: number;
-}) {
+  activeCard,
+  direction,
+  setActiveCard,
+  setDirection,
+}: CardProps) {
   const [isActive, setIsActive] = useState(false);
   const x = useMotionValue(0);
-  // const offset = index - activeCard;
-
   const rangeDeg = useMemo(() => {
     const deg = [-10, 10];
     return Math.random() * (deg[1] - deg[0]) + deg[0];
@@ -30,6 +40,33 @@ export function Card({
     [-200, 0, 200],
     [rangeDeg - 5, rangeDeg, rangeDeg + 5]
   );
+  // const offset = index - activeCard;
+
+  useEffect(() => {
+    if (direction === "shuffle" && index >= activeCard) {
+      animate(x, 1000, {
+        type: "spring",
+        stiffness: 400,
+        damping: 30,
+        delay: index * 0.1,
+      });
+    }
+    if (index === activeCard - 1 && direction === "next") {
+      animate(x, 1000, {
+        type: "spring",
+        stiffness: 400,
+        damping: 30,
+      });
+    }
+
+    if (index === activeCard && direction === "prev") {
+      animate(x, 0, {
+        type: "spring",
+        stiffness: 400,
+        damping: 30,
+      });
+    }
+  }, [activeCard, direction, index, x]);
 
   useEffect(() => {
     if (resetCount > 0) {
@@ -57,6 +94,8 @@ export function Card({
     }
 
     if (currentX < -100) {
+      setDirection(null);
+      setActiveCard(index + 1);
       animate(x, -screenWidth - 500, {
         type: "spring",
         stiffness: 400,
@@ -67,6 +106,8 @@ export function Card({
     }
 
     if (currentX > 100) {
+      setDirection(null);
+      setActiveCard(index + 1);
       animate(x, screenWidth + 500, {
         type: "spring",
         stiffness: 400,
